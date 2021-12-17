769024feb52fbad5aa23bee93debe15e359c97e4
Vibhu Gautam <vibhugautam51@gmail.com>
2021-12-17 13:45:01 +0530

    first commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,26 +2,35 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
-
 const app = express();
 app.use(bodyParser.json());
 
+const events = []
+
 app.post("/events", (req, res) => {
-    const event = req.body;
+  const event = req.body;
 
-    axios.post("http://localhost4000/events", event).catch(err => {
-        console.log(err.message);
-    });
-    axios.post("http://localhost4001/events", event).catch(err => {
-        console.log(err.message);
-    });
-    axios.post("http://localhost4002/events", event).catch(err => {
-        console.log(err.message);
-    });
+  events.push(event);
 
-    res.send({status: "OK"});
+  axios.post("http://posts-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  res.send({ status: "OK" });
 });
 
-app.listen(4005,() => {
-    console.log("Listening on 4005");
+app.get("/events",(req, res) => {
+  res.send(events);
+})
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
 });
